5f108670648d1f1b012634e574f8ac1f9efd2791
Ahmed Salem <ahmedsalem2002m@gmail.com>
2025-08-08 10:31:33 +0300

    text form field validate

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Pacifico"),
+      // theme: ThemeData(fontFamily: "Pacifico"),
       debugShowCheckedModeBanner: false,
       home: HomeView(),
     );
   }
 }
 
-class Home_2 extends StatelessWidget {
-  const Home_2({super.key});
+class HomeView extends StatelessWidget {
+  HomeView({super.key});
+
+  Map<String, String> countriesCodes = {
+    'Egypt': '+20',
+    'KSA': '+966',
+    'UAE': '+971',
+  };
+  TextEditingController email = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,185 +35,77 @@ class Home_2 extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                margin: EdgeInsets.all(8),
-                width: 300,
-                height: 250,
-                child: Image.asset("assets/images/eat.jpg"),
-              ),
-            ),
-            Text("Ahmed Salem"),
-            TextFormField(
-              keyboardType: TextInputType.name,
-              style: TextStyle(fontSize: 12),
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                suffixIcon: IconButton(onPressed: (){
-                  print("Ali");
-                }, icon: Icon(Icons.lock)),
-                labelText: "Enter name",
-                border: OutlineInputBorder(
-
-                ),
-              ),
-
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   leading: Icon(Icons.arrow_back_ios_sharp, color: Colors.green),
-      //   title: Text("Home", style: TextStyle(color: Colors.blue)),
-      //   actions: [
-      //     Padding(
-      //       padding: const EdgeInsets.all(8.0),
-      //       child: Icon(Icons.person),
-      //     ),
-      //   ],
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 56),
-            SizedBox(
-              height: 160,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => HorItem(),
-                itemCount: 10,
-              ),
-            ),
-            SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) => VerticalItem(),
-                itemCount: 10,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class VerticalItem extends StatelessWidget {
-  const VerticalItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 25),
-      padding: EdgeInsets.all(8),
-      height: 120,
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(color: Colors.blueGrey, blurRadius: 3, spreadRadius: 7),
-        ],
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-
-      child: Row(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://septemberfarmcheese.b-cdn.net/wp-content/uploads/Blogs/Homemade-Pizza/homemade-pizza-monterey-jack-cheese.jpg",
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "product name",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("description"),
-                      Text("02/08/2025", style: TextStyle(color: Colors.red)),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class HorItem extends StatelessWidget {
-  const HorItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://septemberfarmcheese.b-cdn.net/wp-content/uploads/Blogs/Homemade-Pizza/homemade-pizza-monterey-jack-cheese.jpg",
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Form(
+          key: formKey,
+          child: Column(
             children: [
-              Text(
-                "Ahmed Salem",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  width: 300,
+                  height: 250,
+                  child: Image.asset("assets/images/eat.jpg"),
+                ),
               ),
-              SizedBox(width: 5),
-              CircleAvatar(radius: 8, backgroundColor: Colors.green),
+              Text("Ahmed Salem"),
+              TextFormField(
+                controller: email,
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    return null;
+                  }
+                  return "field is required";
+                },
+                keyboardType: TextInputType.name,
+                style: TextStyle(fontSize: 12),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.lock),
+                  ),
+                  labelText: "Enter email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              DropdownButtonFormField(
+                items: countriesCodes
+                    .map(
+                      (String countryName, countryCode) => MapEntry(
+                        countryName,
+                        DropdownMenuItem(
+                          value: countryCode,
+                          child: Row(
+                            children: [
+                              Icon(Icons.flag),
+                              Text(countryName),
+                              Text(countryCode),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                    .values
+                    .toList(),
+                onChanged: (value) {
+                  print(value);
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  formKey.currentState!.validate();
+                  print(email.text);
+                },
+                child: Text("press"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
