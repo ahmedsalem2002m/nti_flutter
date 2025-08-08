3305892f57a3ccddec258af44fb3e03203916754
Ahmed Salem <ahmedsalem2002m@gmail.com>
2025-08-08 15:30:41 +0300

    stack (shop app login)

--- lib/main.dart
+++ lib/main.dart
@@ -28,47 +28,94 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  Map<String, String> countriesCodes = {
-    'Egypt': '+20',
-    'KSA': '+966',
-    'UAE': '+971',
-  };
-  TextEditingController email = TextEditingController();
-  var formKey = GlobalKey<FormState>();
-  String? dropdown;
-
-  bool switchValue = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        alignment: Alignment.center,
+        alignment: Alignment.bottomCenter,
         children: [
           SizedBox.expand(
-            child: Image.asset("assets/images/eat.jpg", fit: BoxFit.cover),
+            child: Image.asset(
+              "assets/images/unsplash_fouVDmGXoPI.png",
+              fit: BoxFit.cover,
+            ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Ahmed",
-                style: TextStyle(
-                  color: Colors.teal,
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 42),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                  colors: [
+                Colors.black.withAlpha(200),
+                Colors.black.withAlpha(5),
+              ])
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "You want",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              Text(
-                "Ali",
-                style: TextStyle(
-                  color: Colors.teal,
-                  fontSize: 40,
-                  fontWeight: FontWeight.w900,
+                Text(
+                  "Authentic, here",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
+                Text(
+                  "You go!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "find it here, but it now!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: 45,
+                  color: Colors.red,
+                  child: MaterialButton(onPressed: (){
 
-            ],
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),),
+                ),
+                SizedBox(height: 8,),
+                Container(
+                  width: double.infinity,
+                  height: 45,
+                  color: Colors.white,
+                  child: MaterialButton(onPressed: (){
+                  },
+                  child: Text(
+                    "Register",
+                    style: TextStyle(fontSize: 24, color: Colors.red),
+                  ),),
+                ),
+                SizedBox(height: 10,)
+              ],
+            ),
           ),
         ],
       ),
